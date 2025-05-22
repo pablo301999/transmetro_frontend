6edc2dbb11f65e58b0f86bedf429ba0880e22604
pablo301999 <50727202+pablo301999@users.noreply.github.com>
2025-05-21 18:45:24 -0600

    Se agrega swal fire para alerta en inicio de sesión
    Se agrega swal fire para alerta en inicio de sesión

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoginRequest } from 'src/app/models/login-request.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -14,15 +15,22 @@ export class LoginComponent {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  iniciarSesion() {
-    this.authService.login(this.datos).subscribe({
-      next: res => {
-        this.authService.guardarToken(res.token);
-        this.router.navigate(['/usuarios']);
-      },
-      error: () => {
-        this.error = 'Credenciales incorrectas.';
-      }
-    });
-  }
+iniciarSesion() {
+  this.authService.login(this.datos).subscribe({
+    next: res => {
+      this.authService.guardarToken(res.token);
+      this.router.navigate(['/usuarios']);
+    },
+    error: () => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Credenciales incorrectas.',
+        confirmButtonColor: '#d33',
+        confirmButtonText: 'Intentar de nuevo'
+      });
+    }
+  });
+}
+
 }
